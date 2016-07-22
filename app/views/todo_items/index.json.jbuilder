91ad7d482aedba5b8026e4f1ebbeb7d543ad9f8c
Trevor Allen <trevor.allen@valiance.io>
2016-07-22 16:57:26 -0400

    RAR Module 2 assignment solution. Completely recreated from scratch as part of Module 4 assignment; somehow the 'backup' copy of this assignment's solution was discarded or overwritten by an essentially-new version of the app. Solution repeated and used as starting point for Module 4.

--- app/views/todo_items/index.json.jbuilder
+++ app/views/todo_items/index.json.jbuilder
@@ -1,4 +1,1 @@
-json.array!(@todo_items) do |todo_item|
-  json.extract! todo_item, :id, :due_date, :title, :description, :completed
-  json.url todo_item_url(todo_item, format: :json)
-end
+json.array! @todo_items, partial: 'todo_items/todo_item', as: :todo_item
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+User.create! [
+  { username: "Fiorina", password_digest: '123'},
+  { username: "Trump", password_digest: '123'},
+  { username: "Carson", password_digest: '123'},
+  { username: "Clinton", password_digest: '123'},
+]
+Profile.destroy_all
+Profile.create! [
+  {first_name: "Carly", last_name: "Fiorina", gender: "female", birth_year:1954 },
+  {first_name: "Donald", last_name: "Trump", gender: "male", birth_year:1946 },
+  {first_name: "Ben", last_name: "Carson", gender: "male", birth_year:1951 },
+  {first_name: "Hillary", last_name: "Clinton", gender: "female", birth_year:1947 },
+]
+
+TodoList.destroy_all
+# TodoList.create! [
+#   { user: User.find(username:"Fiorina") , list_name: "mylist", list_due_date: Date.today+1.year },
+#   { user: User.find(username:"Trump") , list_name: "list_Trump", list_due_date: Date.today+1.year },
+#   { user: User.find(username:"Carson") , list_name: "list_Carson", list_due_date: Date.today+1.year },
+#   { user: User.find(username:"Clinton") , list_name: "list_Clinton", list_due_date: Date.today+1.year },
+# ]
+User.all.each do  |u|
+  TodoList.create( {user:u, list_name: "mylist", list_due_date: Date.today+1.year } )
+end
+
+TodoList.all.each do  |tl|
+  # TodoItem.create! [
+  tl.todo_items.create! [
+    { title: "Task 1", due_date: Date.today+1.year, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: Date.today+1.year, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: Date.today+1.year, description: "learn Action Pack TEST", completed: true},
+    { title: "Task 4", due_date: Date.today+1.year, description: "learn Action Pack TEST", completed: true},
+    { title: "Task 5", due_date: Date.today+1.year, description: "learn Action Pack TEST", completed: true},
+  ]
+end
